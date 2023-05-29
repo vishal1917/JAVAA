517c2e2610ee1c32af1cd1dfab78f27062a3e2a6
vishal1917 <107452919+vishal1917@users.noreply.github.com>
2023-05-29 10:41:26 +0530

    Create Variable.java

---
+++ Variable.java
@@ -0,0 +1,14 @@
+public class VARIABLE {
+    public static void main(String[] args) {
+        
+     int m=60;
+     String name="Vishal";
+     float myFloatNum= 6.9f;
+
+    char myLetter='d';
+System.out.println(myFloatNum);
+System.out.println(myLetter);
+  System.out.println(name);
+     System.out.println(m);
+    }
+}
