76d86b327bdfa9659e695880ee4210c80b92c257
vishal1917 <107452919+vishal1917@users.noreply.github.com>
2023-05-29 16:29:11 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,20 @@
+public class STRING {
+    public static void main(String[] args) {
+        //String txt= "aSDFGHJ";
+       // System.out.println("the length of the string is:" +txt.length());
+        //System.out.println(txt.toUpperCase());
+        //System.out.println(txt.toLowerCase());
+      //  String firstName="vishal";
+        //String lastName="sen";
+        //System.out.println(firstName .concat (lastName));
+       // String x="10";
+        //String y="20";
+        //String z=x+y;
+        //System.out.println(z);
+        String x="10";
+        int y=10;
+        String z=x+y;
+        System.out.println(z);
+    }
+    
+}
