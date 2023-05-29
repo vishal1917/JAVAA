f2c3fb8ec94ac9c5ab13822c4cd3e575a58298de
vishal1917 <107452919+vishal1917@users.noreply.github.com>
2023-05-29 15:57:15 +0530

    Create Operator.java

---
+++ Operator.java
@@ -0,0 +1,17 @@
+public class OPERATOR {
+    public static void main(String[] args) {
+       
+        //int x=10;
+        //x += 10;
+        //int x= 5;
+        //int y= 2;
+//int x=10;
+//int y=20;
+ //int x=10;
+// x-=10;
+int x=10;
+x*=10;
+        System.out.println(x);
+      }
+    }
+    
