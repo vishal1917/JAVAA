ccd3845231c053c4a6085f5f79bbec3aacb5da43
vishal1917 <107452919+vishal1917@users.noreply.github.com>
2023-05-31 15:05:09 +0530

    Create FORLOOP.java

---
+++ FORLOOP.java
@@ -0,0 +1,25 @@
+public class FORLOOP {
+    public static void main(String[] args) {
+        //for(int i=1; i<=3; i++){
+
+        
+          //for(int j=1; j<=5; j++){
+         // System.out.println("*");
+          //}
+           // System.out.println( );
+          
+        //}
+//int arr[]= {10,12,16,28,18};
+  //     for(int i:arr)
+    //   {
+//System.out.println(i);
+  //         }
+int i=1;
+while(1<2)
+    {
+        System.out.println("vishal");
+        i++;
+    }    
+}
+    }
+ 
