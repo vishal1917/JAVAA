7a13b3714d0df20790ee587ac1e2fa458db22aa7
vishal1917 <107452919+vishal1917@users.noreply.github.com>
2023-05-31 14:05:23 +0530

    Create ARRAY.java

---
+++ ARRAY.java
@@ -0,0 +1,8 @@
+public class ARRAY {
+    public static void main(String[] args) {
+        String[] Cars = {"BMW", "VOLVO", "VERNA"};
+        //  System.out.println(Cars.length);
+        Cars[1]= "vishal";
+        System.out.println(Cars[1]);
+    }
+}
