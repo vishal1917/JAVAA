51beab714c44621ad0b00605c81f8fce29625fd1
vishal1917 <107452919+vishal1917@users.noreply.github.com>
2023-06-02 17:10:28 +0530

    Create largeno.java

---
+++ largeno.java
@@ -0,0 +1,16 @@
+public class largeno {
+    public static void main(String[] args) {
+        int a,b;
+        a=3;
+        b=10;
+
+        if(a<b){
+            System.out.println("First num is large");
+        }
+        
+        else{
+            System.out.println("Second num is large");
+        }
+        System.out.println("vishal");
+    }
+}
